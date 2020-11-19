4487e0fd052292578dfa411b1ed1f87a20f9021e
juhimulchandani <juhimulchandani1998@gmail.com>
2020-11-19 17:51:08 +0530

    second commit - new class testapp

--- MyJavaProject/src/com/lti/demo/ClientApp.java
+++ MyJavaProject/src/com/lti/demo/ClientApp.java
@@ -6,6 +6,8 @@ public class ClientApp {
 		// TODO Auto-generated method stub
 		Person p = new Person("Jay",101,5000);
 		System.out.println(p);
+		Person p1 = new Person("Vijay",102,6000);
+		System.out.println(p1);
 	}
 
 }
